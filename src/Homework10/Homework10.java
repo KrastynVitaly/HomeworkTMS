d3bdfe5dbfbee28b886d2f44417b06f87c292dc0
KrastynVitaly <36473628+KrastynVitaly@users.noreply.github.com>
2025-07-16 23:19:03 +0300

    Homework 10 - Final

--- src/Homework10/Homework10.java
+++ src/Homework10/Homework10.java
@@ -11,19 +11,30 @@ public class Homework10 {
         Person person = new Person("Иванов", "Иван", 20);
         User user1 = new User("Den99");
         User user2 = new User("Tom", 567, person);
-        User user3 = new User("Den008", 123 ,person);
-        User user4 = user3.deepClone();
+        User user3 = new User("Tom", 567, person);
+        User user4 = new User("Den008", 123 ,person);
+        User user5 = user3.deepClone();
+        User user6 = user3.clone();
 
+        System.out.println("Users:");
         System.out.println(user1);
         System.out.println(user2);
         System.out.println(user3);
         System.out.println(user4);
+        System.out.println(user5);
+        System.out.println();
 
+        System.out.println("Hashcode пользователей:");
         System.out.println(user1.hashCode());
         System.out.println(user2.hashCode());
         System.out.println(user3.hashCode());
         System.out.println(user4.hashCode());
+        System.out.println(user5.hashCode());
+        System.out.println();
 
-        System.out.println(user3.equals(user4));
+        System.out.println("Сравнение user2 и user3");
+        System.out.println(user2.equals(user3)); // Выводит true
+        System.out.println(user3.equals(user5)); // Выводит false
+
     }
 }
