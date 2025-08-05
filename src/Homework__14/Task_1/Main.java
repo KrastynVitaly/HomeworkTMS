25a67d6b27c90c7252fafa2f55bfa19a2135f367
KrastynVitaly <36473628+KrastynVitaly@users.noreply.github.com>
2025-08-05 06:27:07 +0300

    14-1

--- src/Homework__14/Task_1/Main.java
+++ src/Homework__14/Task_1/Main.java
@@ -1,24 +1,29 @@
 package Homework__14.Task_1;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
-        String longest_word = "";
-        String current;
-        Scanner sc = new Scanner(new File("romeo-and-juliet.txt"));
-        while (sc.hasNext()) {
-            current = sc.next();
-            if (current.length() > longest_word.length()) {
-                longest_word = current;
-            }
+        FileInputStream fis = new FileInputStream("romeo-and-juliet.txt");
+        int i = -1;
+        StringBuilder text = new StringBuilder();
+        while ((i = fis.read()) != -1) {
+            text.append((char) i);
         }
+        fis.close();
+
+        String[] words = String.valueOf(text).split("[^a-zA-Z]");
+        String longest_word = "";
+        for (String word : words )
+            if (word.length() > longest_word.length()) {
+                longest_word = word;
+            }
+
         System.out.println("\n" + longest_word + "\n");
 
         try (FileOutputStream fos = new FileOutputStream("longest-word.txt")) {
-            fos.write(longest_word.getBytes());
+            fos.write(longest_word.getBytes(), 0, longest_word.length());
+            fos.close();
 
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
