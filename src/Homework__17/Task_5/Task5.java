56428cd39306ad24348f0ad06128faa11a30d18f
KrastynVitaly <36473628+KrastynVitaly@users.noreply.github.com>
2025-08-14 10:46:03 +0300

    Homework 18 - Task 1

--- src/Homework__17/Task_5/Task5.java
+++ src/Homework__17/Task_5/Task5.java
@@ -1,6 +1,5 @@
 package Homework__17.Task_5;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
---
+++ src/Homework__18/Task_1/Task1.java
@@ -0,0 +1,38 @@
+package Homework__18.Task_1;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class Task1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> numberArray = new ArrayList<>();
+        numberArray.add(12);
+        numberArray.add(15);
+        numberArray.add(4);
+        numberArray.add(35);
+        numberArray.add(63);
+        numberArray.add(84);
+        numberArray.add(25);
+        numberArray.add(7);
+        numberArray.add(23);
+        numberArray.add(94);
+
+        System.out.println("Исходные значения в numberArray: ");
+        for (int element : numberArray) {
+            System.out.print(element + " ");
+        }
+        System.out.println("\n");
+
+        Optional<Integer> objNumArray = numberArray.stream()
+                .distinct()
+                .filter((n) -> (n % 2) == 0)
+                .reduce(Integer::sum);
+        int outputArray = numberArray.stream()
+                .distinct()
+                .filter((n) -> (n % 2) == 0)
+                .reduce(0, Integer::sum);
+        objNumArray.ifPresent(integer -> System.out.println("Сумма как Optional: " + integer));
+        System.out.println("Сумма как int: " + outputArray);
+
+    }
+}
