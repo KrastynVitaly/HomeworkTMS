585454b4313355db87500318547bb6ab33828645
Comp <krastynvitaly@gmail.com>
2025-10-14 21:45:52 +0300

    Homework 24 in HomeworkTMS

---
+++ Homework 24/src/main/java/MoscowTimeServlet.java
@@ -0,0 +1,24 @@
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class MoscowTimeServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        Writer out = resp.getWriter();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String path = req.getServletPath();
+        String city = "Moscow";
+        ZoneId zoneId = ZoneId.of("Europe/Moscow");
+        ZonedDateTime time = ZonedDateTime.now(zoneId);
+        String formattedTime = time.format(formatter);
+        out.append("<html> Hello " + city + ": " + formattedTime + " :)</html>");
+        out.close();
+    }
+}
