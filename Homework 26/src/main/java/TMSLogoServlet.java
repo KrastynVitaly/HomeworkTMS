29f44ffe2068370d8e3a186d88e4741b5fa1e73b
Comp <krastynvitaly@gmail.com>
2025-11-06 20:59:18 +0300

    Superhero bootsrap start

--- Homework 26/src/main/java/TMSLogoServlet.java
+++ Homework 26/src/main/java/TMSLogoServlet.java
@@ -10,6 +10,6 @@ import java.io.IOException;
 public class TMSLogoServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/index.html").forward(req, resp);
+        getServletContext().getRequestDispatcher("/logotms.html").forward(req, resp);
     }
 }
